77b39b54de6308cd8accc167373b9c6687ca3861
Glen Maddern <glenmaddern@gmail.com>
2017-08-30 13:46:34 +1000

    making the header a router link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import logo from '../assets/logo.svg'
 
 export default () => (
   <nav className="Nav mb3">
-    <a href="/" className="Logo">
+    <Link to="/" className="Logo">
       <img src={ logo } alt="pithy.af logo" />
-    </a>
+    </Link>
     <a className="NavLink" href="#about">About</a>
     <a className="NavLink" href="#submit">Submit a quote</a>
   </nav>
